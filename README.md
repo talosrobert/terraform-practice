3d74119e37c11d3b50d4334d24fe91d45ce1f9a7
talosrobert <29042337+talosrobert@users.noreply.github.com>
2022-02-20 07:27:07 +0100

    Added NSG definitions.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # terraform-practice
 Terraform Practice Repository
+
+## build new image with podman
+podman build -f Dockerfile --tag aztf
+
+## run the container and mount the config folder
+podman container run --rm -v ./config:/root/config:z -it aztf
--- config/main.tf
+++ config/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "rg" {
   location = "westeurope"
 
   tags = {
-    environment = "testing-westeurope"
+    Environment = "Testing"
   }
 }
 
@@ -31,17 +31,88 @@ resource "azurerm_virtual_network" "vnet" {
   subnet {
     name           = "snet-test-westeurope-001"
     address_prefix = "10.0.1.0/24"
+    security_group = azurerm_network_security_group.nsg1.id
   }
 
   subnet {
     name           = "snet-test-westeurope-002"
     address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.nsg1.id
   }
 
   subnet {
     name           = "snet-test-westeurope-003"
     address_prefix = "10.0.3.0/24"
+    security_group = azurerm_network_security_group.nsg2.id
+  }
+
+  tags = {
+    Environment = "Testing"
   }
 }
 
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "nsg-test-westeurope-001"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
+  security_rule {
+    name                       = "allow-inbound-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.0.1.0/24"
+  }
+
+  tags = {
+    Environment = "Testing"
+  }
+}
+
+resource "azurerm_network_security_group" "nsg2" {
+  name                = "nsg-test-westeurope-002"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "allow-inbound-http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.0.3.0/24"
+  }
+
+  security_rule {
+    name                       = "allow-inbound-htts"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.0.3.0/24"
+  }
+
+  tags = {
+    Environment = "Testing"
+  }
+}
+
+resource "azurerm_network_watcher" "nw" {
+  name                = "nw-test-westeurope-001"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    Environment = "Testing"
+  }
+}
